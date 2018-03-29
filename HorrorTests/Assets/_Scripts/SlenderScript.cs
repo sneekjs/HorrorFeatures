2a1b8e0b7587eb547bf2582182383d4b4ccc88da
sneekjs <sneekjs@users.noreply.github.com>
2018-03-29 11:16:02 +0200

    Slender looks cool

--- HorrorTests/Assets/_Scripts/SlenderScript.cs
+++ HorrorTests/Assets/_Scripts/SlenderScript.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class SlenderScript : MonoBehaviour {
+
+    public VideoPlayer staticVideo;
 
     public bool isVisible;
 
@@ -20,15 +24,26 @@ public class SlenderScript : MonoBehaviour {
      
 	void Start () {
         target = GameObject.Find("Player");
+        staticVideo.gameObject.SetActive(false);
 	}
 	
 	void Update () {
+
+        //Debug.Log(1-(Vector3.Distance(transform.position, target.transform.position)/10));
         if (isVisible == true)
         {
+            if (1 - (Vector3.Distance(transform.position, target.transform.position) / 10) >0.05f)
+            {
+                staticVideo.gameObject.SetActive(true);
+            }
+            else staticVideo.gameObject.SetActive(false);
 
         }
         if (isVisible == false)
         {
+            staticVideo.gameObject.SetActive(false);
+
+
             Debug.Log("InVisible");
             playerDir = target.transform.position - transform.position;
             playerDir.y = 0;
